95668baf3c863e0d7ea823708906788ed54aae57
Jayraj Shah <jayrajcshah@gmail.com>
2020-07-31 02:06:20 +0530

    Added basic CRA template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        color: "#5a5a5a",
+      }}
+    >
+      <h1>A Simple Scraper</h1>
+      <p>Using React and Service Workers</p>
+      <p>
+        Go to Inspect (ctrl+shift+i) and navigate to application tab to check
+        the ServiceWorker
+      </p>
+      <p>then check the console to see the output</p>
+      <h2>Developed By: Jayraj Shah</h2>
     </div>
   );
 }
